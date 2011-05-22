4038c9746d0fbf4c67abce30fa424e02643f7777
Guy Rozendorn <guyr@infinidat.com>
2011-05-22 17:26:30 +0300

    openssl hooks: executing the batch files with the proper environment

--- src/hooks/windows.py
+++ src/hooks/windows.py
@@ -1,10 +1,14 @@
 __import__("pkg_resources").declare_namespace(__name__)
 
-import os
+import os, subprocess
 
-def openssl_pre_make(options, buildout, version):
-    os.system(r'ms\do_ms.bat')
+def _execute(cmd, env):
+    process = subprocess.Popen(cmd.split(), env=env)
+    return process.wait()
 
-def openssl_pre_make64(options, buildout, version):
-    os.system(r'ms\do_win64a')
+def openssl_pre_make(options, buildout, environ):
+    _execute(r'ms\do_ms.bat', environ)
+
+def openssl_pre_make64(options, buildout, environ):
+    execute(r'ms\do_win64a', environ)
 
