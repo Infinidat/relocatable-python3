473078e708b60b46ce8f22db2147b31a9922cf48
Guy Rozendorn <guy@rzn.co.il>
2013-08-19 14:31:18 +0300

    TRIVIAL another attempt to workaround sysconfig issues

---
+++ src/hooks/posix.py
@@ -0,0 +1,17 @@
+TRICK = """
+# isolated-python trick
+import sys
+prefix = sys.real_prefix if hasattr(sys, 'real_prefix') else sys.prefix  # virtualenv
+old_prefix = build_time_vars.get("prefix", "_some_path_that_does_not_exist")
+
+for key, value in build_time_vars.items():
+    build_time_vars[key] = value.replace(old_prefix, prefix) if isinstance(value, basestring) else value
+"""
+
+def purge_sysconfigdata(options, buildout, environ):
+    from glob import glob
+    from os import path, curdir
+    dist = path.join(environ.get("PWD"), "dist")
+    [_sysconfigdata] = glob(path.join(dist, "*", "*", "_sysconfigdata.py"))
+    with open(_sysconfigdata, 'a') as fd:
+        fd.write(TRICK)
