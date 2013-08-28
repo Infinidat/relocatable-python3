6b6102b0eb1f84d140fbeb2b87bb711492c85f55
Guy Rozendorn <guy@rzn.co.il>
2013-08-28 12:35:04 +0300

    TRIVIAL skip the readline hook on windows

--- src/hooks/editline.py
+++ src/hooks/editline.py
@@ -1,5 +1,9 @@
 from infi.execute import execute_assert_success
+import os
+
 
 def replace_readline_with_editline(options, buildout, environ):
+    if os.name == "nt":
+        return
     command = "find . -type f -exec perl -p -i -e s/-lreadline/-ledit/g {} ;"
     execute_assert_success(command.split())
