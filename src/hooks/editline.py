a357364eda2395f873c421897415ff120c1e2004
Guy Rozendorn <guy@rzn.co.il>
2013-08-28 18:05:17 +0300

    TRIVIAL pre-configure-hook for editline

--- src/hooks/editline.py
+++ src/hooks/editline.py
@@ -7,3 +7,11 @@ def replace_readline_with_editline(options, buildout, environ):
         return
     command = "find . -type f -exec perl -p -i -e s/-lreadline/-ledit/g {} ;"
     execute_assert_success(command.split())
+
+
+def editline_preconfigure_hook(options, buildout, environ):
+    env = {key: value for key, value in os.environ.iteritems()}
+    env['LC_ALL'] = 'C'
+    execute_assert_success(["aclocal"], env=env)
+    execute_assert_success(["autoconf"], env=env)
+    execute_assert_success(["automake"], env=env)
