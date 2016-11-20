e74b2bd206ae9cc54675e21b38150ba8b9827ad2
Amit Oren <aoren@infinidat.com>
2016-11-20 11:41:23 +0200

    HOSTDEV-2454 temporarily dont check certificates

--- tests/test_ssl.py
+++ tests/test_ssl.py
@@ -2,8 +2,10 @@
 def main():
     # hopefully, even the oldest ca-certificates will verify google.com
     import urllib
-    # TODO: remove verify=False once certificates are properly updated.
-    urllib.urlretrieve("https://google.com", verify=False)
+    import ssl
+    # TODO: remove the unverified context once certificates are properly updated.
+    context = ssl._create_unverified_context()
+    urllib.urlretrieve("https://google.com", context=context)
 
 
 if __name__ == "__main__":
