70be1a855eb14a37d99ec58e37b1671c181b3ff7
Amit Oren <aoren@infinidat.com>
2016-12-04 12:07:50 +0200

    TRIVIAL getting SSL test back to normal

--- tests/test_ssl.py
+++ tests/test_ssl.py
@@ -2,10 +2,7 @@
 def main():
     # hopefully, even the oldest ca-certificates will verify google.com
     import urllib
-    import ssl
-    # TODO: remove the unverified context once certificates are properly updated.
-    context = ssl._create_unverified_context()
-    urllib.urlretrieve("https://google.com", context=context)
+    urllib.urlretrieve("https://google.com")
 
 
 if __name__ == "__main__":
