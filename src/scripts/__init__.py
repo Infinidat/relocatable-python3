5453f55f4811c7d222a79255f7229da4aa345b70
Arnon Yaari <arnony@infinidat.com>
2018-12-12 15:07:57 +0200

    remove libvirt
    The libvirt we use is relatively old and does not compile on newer gccs
    Using a newer version is too much work

--- src/scripts/__init__.py
+++ src/scripts/__init__.py
@@ -50,12 +50,7 @@ def build():
                 buildout_file = 'buildout-build-redhat-ppc64.cfg'
             elif 'i386' in arch:
                 buildout_file = 'buildout-build-redhat-32bit.cfg'
-            elif int(version.split(".")[0]) > 6 or \
-                (int(version.split(".")[0]) == 6 and int(version.split(".")[1]) >= 4):
-                # arch is 64 bit and supports libvirt
-                buildout_file = 'buildout-build-redhat-64bit-with-libvirt.cfg'
             else:
-                # arch is 64 bit
                 buildout_file = 'buildout-build-redhat-64bit.cfg'
         if dist_name in ['suse']:
             arch = execute_assert_success(["uname", "-i"]).get_stdout().lower()
