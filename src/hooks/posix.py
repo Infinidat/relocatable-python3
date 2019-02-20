72f42e97391f8bd2dd7f3dff4f6cbe382eab11e9
Arnon Yaari <arnony@infinidat.com>
2019-02-20 10:54:21 +0200

    HOSTDEV-3178 add more rpaths to sysconfigdata
    This helps packages installed in site-packages
    correctly link to the lib dir in Solaris

--- src/hooks/posix.py
+++ src/hooks/posix.py
@@ -28,12 +28,14 @@ def purge_sysconfigdata(path):
 
 
 def fix_linker_rpath(path):
-    # we want to link against the .so files in python/lib, but $ORIGIN may point to
-    # python/lib/python2.7/site-packages/<package-root>/<package-src>
+    # we want to link against the .so files in python/lib, but in Solaris
+    # $ORIGIN may point to the package .so file location (instead of the python
+    # binary location), and these files may be deep under site-packages
     # (e.g. python/lib/python2.7/site-packages/lxml-3.4.1-py2.7-linux-i686.egg/lxml)
-    # so we add $ORIGIN/../../../.. to rpath in linker options
+    # so we add some $ORIGIN/../../../.... to rpath in linker options
     src_str = r"-Wl,-rpath,\\$ORIGIN/../.."
-    dst_str = r"-Wl,-rpath,\\$ORIGIN/../..,-rpath,\\$ORIGIN/../../../.."
+    rpaths = ''.join([",-rpath,\\$ORIGIN" + ("/.." * i) for i in range(3, 8)])
+    dst_str = src_str + rpaths
     with open(path, 'r') as fd:
         data = fd.read()
     data = data.replace(src_str, dst_str)
