aabf1b9cd30b96dc6e59fd613b0dd6d754b4dace
Ory Jonay <ojonay@infinidat.com>
2019-11-21 20:06:54 +0200

    Fix build on SUSE > 15
    Fixed issue where when building on SUSE version > 15
    it used the wrong buildout configuration

--- src/scripts/__init__.py
+++ src/scripts/__init__.py
@@ -67,7 +67,7 @@ def build():
                 buildout_file = 'buildout-build-suse-ppc64le.cfg'
             elif 'ppc64' in arch:
                 buildout_file = 'buildout-build-suse-ppc64.cfg'
-            elif version == "15":
+            elif version >= "15":
                 buildout_file = 'buildout-build-suse-15.cfg'
     elif system() == 'Darwin':
         from platform import mac_ver
