6f35626664e72fff9091c04d774fe27310f357f6
Kobi Tal <ktal@infinidat.com>
2021-12-02 15:52:08 +0200

    Fix test

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -1,8 +1,6 @@
 def main():
     import subprocess
     import sys
-    res = subprocess.run([sys.executable, "-c", "import os; print(os.environ['LIBPATH'])"])
-    assert(res.returncode == 0)
     res = subprocess.run([sys.executable, "tests/test_dynload_imports.py"])
     assert(res.returncode == 0)
 
