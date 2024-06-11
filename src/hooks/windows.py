85d315d62c1f477e03803165de936c89121e8e6f
Jenkins CI <noreply@infinidat.com>
2024-06-11 20:14:17 +0100

    hooks
    Signed-off-by: Jenkins CI <noreply@infinidat.com>

--- src/hooks/windows.py
+++ src/hooks/windows.py
@@ -17,15 +17,21 @@ def _system(cmd):
 def _mkdir(path):
     _system('install -v -d "%s"' % path)
 
-def _copy(src, dst):
+def _todo(act, src, dst):
     if isinstance(src, str):
-        _system('cp -f -r -v "%s" "%s"' % (src, dst))
+        _system('%s "%s" "%s"' % (act, src, dst))
     elif isinstance(src, (list, tuple)):
         _mkdir(dst)
         for item in src:
-            _system('cp -f -r -v "%s" "%s"' % (item, dst))
+            _system('%s "%s" "%s"' % (act, item, dst))
     else:
         raise Exception('Unexpected source type %s' % type(src))
+
+def _copy(src, dst):
+    _todo('cp -f -r -v', src, dst)
+
+def _move(src, dst):
+    _todo('mv -f -v', src, dst)
 
 def xz_post_make(options, buildout, environ):
     prefix = environ['PREFIX']
@@ -53,23 +59,11 @@ def libiconv_post_make(options, buildout, environ):
     _copy(glob.glob(os.path.join(suffix, '*.exe')),
           os.path.join(prefix, 'bin'))
 
-def _libffi_post_make(platform_name, prefix):
-    prefix = environ['PREFIX']
+def libevent_post_make(options, buildout, environ):
     0/0
-    suffix = os.path.join()
-    python_source_path = path.abspath(path.join(os.curdir, path.pardir)).replace(os.path.sep, '/')
-    os.system('cp -fvr %s/externals/libffi/%s/include/*.h %s/include' % (python_source_path, platform_name, prefix))
-    os.system('cp -fvr %s/externals/libffi/%s/*.lib %s/lib' % (python_source_path, platform_name, prefix))
-    os.system('cp -fvr %s/externals/libffi/%s/*.dll %s/lib' % (python_source_path, platform_name, prefix))
+    prefix = environ['PREFIX']
 
-def libffi_post_make(options, buildout, environ):
-    prefix = environ['PREFIX'].replace(os.path.sep, '/')
-    _libffi_post_make('amd64', prefix)
-
-def tcl_post_make(options, buildout, environ):
-    prefix = environ['PREFIX'].replace(os.path.sep, '/')
-    os.system('chmod -R 744 %s/lib/tcl8.6/tzdata' % prefix)
-    os.system('chmod -R 744 %s/lib/tcl8.6/msgs' % prefix)
+    os.system('cp -fvr *lib %s/lib' % prefix)
 
 class PythonPostMake(object):
     def __init__(self, environ):
@@ -91,81 +85,72 @@ class PythonPostMake(object):
         self.copy_crt()
 
     def move_dlls(self):
-        items = glob.glob(path.join(self.prefix, 'bin', '*.dll'))
-        items += glob.glob(path.join(self.pcbuild_path, '*.dll'))
-        items += glob.glob(path.join(self.prefix, 'lib', '*.pdb'))
-        items += glob.glob(path.join(self.pcbuild_path, '*.pyd'))
+        src = glob.glob(path.join(self.prefix, 'bin', '*.dll'))
+        src = [dll for dll in src if 'python' not in dll]
+        src += glob.glob(path.join(self.pcbuild_path, '*.dll'))
+        src += glob.glob(path.join(self.prefix, 'lib', '*.pdb'))
+        src += glob.glob(path.join(self.pcbuild_path, '*.pyd'))
         dst = path.join(self.prefix, 'DLLs')
-        mkdir(dst)
-        for item in items:
-            if 'python311.dll' in item:
-                continue
-            _system('mv -fv %s %s' % (item, dst))
+        _move(src, dst)
 
     def move_bins(self):
-        items = glob.glob(path.join(self.pcbuild_path, '*.exe'))
-        items += glob.glob(path.join(self.pcbuild_path, '*.ico'))
+        src = glob.glob(path.join(self.pcbuild_path, '*.exe'))
+        src += glob.glob(path.join(self.pcbuild_path, '*.ico'))
         dst = path.join(self.prefix, 'bin')
-        mkdir(dst)
-        for item in items:
-            _system('mv -fv %s %s' % (item, dst))
+        _move(src, dst)
 
     def move_libs(self):
-        items = glob.glob(path.join(self.prefix, 'lib', '*.a'))
-        items += glob.glob(path.join(self.prefix, 'lib', '*.lib'))
-        items += glob.glob(path.join(self.pcbuild_path, '*.lib'))
+        src = glob.glob(path.join(self.prefix, 'lib', '*.a'))
+        src += glob.glob(path.join(self.prefix, 'lib', '*.lib'))
+        src += glob.glob(path.join(self.pcbuild_path, '*.lib'))
         dst = path.join(self.prefix, 'libs')
-        mkdir(dst)
-        for item in items:
-            _system('mv -fv %s %s' % (item, dst))
-        items = glob.glob(path.join(self.python_source_path, 'lib', '*'))
+        _move(src, dst)
+        src = glob.glob(path.join(self.python_source_path, 'lib', '*'))
         dst = path.join(self.prefix, 'lib')
-        mkdir(dst)
-        for item in items:
-            _system('mv -fv %s %s' % (item, dst))
+        _move(src, dst)
 
     def move_libffi(self):
         name = 'libffi-3.4.4'
         base = path.join(self.externals, name, self.arch)
-        _system('mv -fv %s %s' % (path.join(base, 'include', '*'),
-                                  path.join(self.prefix, 'include')))
-        _system('mv -fv %s %s' % (path.join(base, '*.lib'),
-                                  path.join(self.prefix, 'libs')))
-        _system('mv -fv %s %s' % (path.join(base, '*.dll'),
-                                  path.join(self.prefix, 'DLLs')))
+        src = glob.glob(path.join(base, 'include', '*.h'))
+        dst = path.join(self.prefix, 'include')
+        _move(src, dst)
+        src = glob.glob(path.join(base, '*.lib'))
+        dst = path.join(self.prefix, 'libs')
+        _move(src, dst)
+        src = glob.glob(path.join(base, '*.dll'))
+        dst = path.join(self.prefix, 'DLLs')
+        _move(src, dst)
 
     def move_openssl(self):
         name = 'openssl-bin-1.1.1w'
         base = path.join(self.externals, name, self.arch)
-        _system('mv -fv %s %s' % (path.join(base, 'include', '*'),
-                                  path.join(self.prefix, 'include')))
-        _system('mv -fv %s %s' % (path.join(base, '*.lib'),
-                                  path.join(self.prefix, 'libs')))
-        _system('mv -fv %s %s' % (path.join(base, '*.dll'),
-                                  path.join(self.prefix, 'DLLs')))
+        src = glob.glob(path.join(base, 'include', 'openssl', '*.h'))
+        dst = path.join(self.prefix, 'include')
+        _move(src, dst)
+        src = glob.glob(path.join(base, '*.lib'))
+        dst = path.join(self.prefix, 'libs')
+        _move(src, dst)
+        src = glob.glob(path.join(base, '*.dll'))
+        dst = path.join(self.prefix, 'DLLs')
+        _move(src, dst)
 
     def move_headers(self):
-        _system('mv -fv %s %s' % (path.join(self.python_source_path, 'Include', '*'),
-                                  path.join(self.prefix, 'include')))
-        _system('mv -fv %s %s' % (path.join(self.python_source_path, 'PC', 'pyconfig.h'),
-                                  path.join(self.prefix, 'include')))
+        src = glob.glob(path.join(self.python_source_path, 'Include', '*.h'))
+        dst = path.join(self.prefix, 'include')
+        _move(src, dst)
+        src = path.join(self.python_source_path, 'PC', 'pyconfig.h')
+        _move(src, dst)
 
     def copy_crt(self):
-        items = glob.glob(path.join(self.environ['WindowsSdkDir'],
-                                    'Redist', self.environ['XSDKVer'],
-                                    'ucrt', 'DLLs', 'x64', '*.dll'))
-        items += glob.glob(path.join(self.environ['VCRoot'],
-                                     'Redist', 'MSVC', '14.16.27012',
-                                     'x64', 'Microsoft.VC141.CRT', '*.dll'))
+        src = glob.glob(path.join(self.environ['WindowsSdkDir'],
+                                  'Redist', self.environ['XSDKVer'],
+                                  'ucrt', 'DLLs', 'x64', '*.dll'))
+        src += glob.glob(path.join(self.environ['VCRoot'],
+                                   'Redist', 'MSVC', '14.16.27012',
+                                   'x64', 'Microsoft.VC141.CRT', '*.dll'))
         dst = path.join(self.prefix, 'bin')
-        mkdir(dst)
-        for item in items:
-            _system('cp -fv "%s" "%s"' % (item, dst))
-
-def libevent_post_make(options, buildout, environ):
-    import os
-    prefix = environ['PREFIX'].replace(os.path.sep, '/')
-    os.system('cp -fvr *lib %s/lib' % prefix)
+        _copy(src, dst)
 
 def python_post_make(options, buildout, environ):
     instance = PythonPostMake(environ)
